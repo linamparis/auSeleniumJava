3380c7103cc8137d036cdfdabfd1cf097a58397f
linamparis <linamparis@gmail.com>
2024-08-29 07:29:20 -0500

    segunda entrega

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,23 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Dimension;
+
 
 public class Selenium {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.setBrowserVersion("128.0.6613.85");
+        driver = new ChromeDriver(options);
+    }
 
 
 
     @Test
-    void manageDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments ("start-maximized");
-        ChromeDriver driver = new ChromeDriver(options);
-
-
-
-
-
-
-
+    void managerDiriver(){
+       driver.manage().window();
     }
+
+    @Test
+    void openWithDimantions(){
+        Dimension dimension = new Dimension(900,600);
+        driver.manage().window().setSize(dimension);
+    }
+
 
 }
