f445dc2cf56e060d06079e16b950311c0608a8b4
linamparis <linamparis@gmail.com>
2024-08-29 15:33:58 -0500

    segunda entrega

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,4 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
+
+
 public class SauceDemoTest {
 
+   @Test
+    public void loginEnSauceDemoSuccessful() {
+        ChromeOptions options = new ChromeOptions();
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).click();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+    }
 
 }
